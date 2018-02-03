8573ee9da6defa8bda3a3fa8e26cbd83d4932e95
KMTaylor023 <kmtaylor023@gmail.com>
2018-02-03 17:48:08 -0500

    finished writing the server and client

--- src/responses.js
+++ src/responses.js
@@ -1,6 +1,7 @@
-const fs = require('fs');  // pull in the file system module
+const fs = require('fs'); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
 const respond = (request, response, content, type, status) => {
   response.writeHead(status, { 'Content-Type': type });
@@ -8,37 +9,126 @@ const respond = (request, response, content, type, status) => {
   response.end();
 };
 
-const getIndex = (request, response) => {
-  respond(request, response, index, 'text/html');
+// creates a json response string, with an id if provided
+const createJSON = (msg, id) => {
+  const json = { msg };
+  if (id) {
+    json.id = id;
+  }
+
+  return JSON.stringify(json);
 };
 
-const success = (request, response) => {
-  respond(request, response, index, 'text/html');
+// creates an xml response string, with id if provided
+const createXML = (msg, id) => {
+  let xml = '<response>';
+
+  if (id) {
+    xml = `${xml} <id> ${id} </id>`;
+  }
+
+  xml = `${xml} <message> ${msg} </message> </response>`;
+
+  return xml;
 };
 
-const badRequest = (request, response) => {
-  respond(request, response, index, 'text/html');
+// creates the response string based on accepted type
+const createResponseString = (msg, id, acceptedTypes) => {
+  if (acceptedTypes[0] === 'text/xml') {
+    return {
+      content: createXML(msg, id),
+      type: 'text/xml',
+    };
+  }
+  return {
+    content: createJSON(msg, id),
+    type: 'application/json',
+  };
 };
 
-const unauthorized = (request, response) => {
-  respond(request, response, index, 'text/html');
+const getIndex = (request, response, acceptedTypes, params) => {
+  respond(request, response, index, 'text/html', 200);
 };
 
-const forbidden = (request, response) => {
-  respond(request, response, index, 'text/html');
+const getCSS = (request, response, acceptedTypes, params) => {
+  respond(request, response, css, 'text/css', 200);
 };
 
-const internal = (request, response) => {
-  respond(request, response, index, 'text/html');
+const success = (request, response, acceptedTypes, params) => {
+  const msg = 'This is a succesful response!';
+
+  const resp = createResponseString(msg, "Success", acceptedTypes);
+  return respond(request, response, resp.content, resp.type, 200);
 };
 
-const notImplemented = (request, response) => {
-  respond(request, response, index, 'text/html');
+const badRequest = (request, response, acceptedTypes, params) => {
+  const msg = "Missing 'valid' query paramter set to true";
+  const id = 'badRequest';
+  if (!params.valid || params.valid !== 'true') {
+    const resp = createResponseString(msg, id, acceptedTypes);
+
+    return respond(request, response, resp.content, resp.type, 400);
+  }
+  return success(request, response, acceptedTypes, params);
 };
 
-const notFound = (request, response) => {
-  respond(request, response, index, 'text/html');
+const unauthorized = (request, response, acceptedTypes, params) => {
+  const msg = "User not authorized! ('loggedIn' query parameter not set to 'yes')";
+  const id = 'unauthorized';
+  if (!params.loggedIn || params.loggedIn !== 'yes') {
+    const resp = createResponseString(msg, id, acceptedTypes);
+
+    return respond(request, response, resp.content, resp.type, 401);
+  }
+
+  return success(request, response, acceptedTypes, params);
+};
+
+const forbidden = (request, response, acceptedTypes, params) => {
+  const msg = "You don't have access to this content";
+  const id = 'forbidden';
+
+  const resp = createResponseString(msg, id, acceptedTypes);
+
+  return respond(request, response, resp.content, resp.type, 403);
+};
+
+const internal = (request, response, acceptedTypes, params) => {
+  const msg = 'Internal Server Error... Something went wrong!';
+  const id = 'InternalServerError';
+
+  const resp = createResponseString(msg, id, acceptedTypes);
+
+  return respond(request, response, resp.content, resp.type, 500);
+};
+
+const notImplemented = (request, response, acceptedTypes, params) => {
+  const msg = 'GET requests for this page not yet implemented';
+  const id = 'notImplemented';
+
+  const resp = createResponseString(msg, id, acceptedTypes);
+
+  return respond(request, response, resp.content, resp.type, 501);
+};
+
+const notFound = (request, response, acceptedTypes, params) => {
+  const msg = 'Error 404: resource not found';
+  const id = 'notFound';
+
+  const resp = createResponseString(msg, id, acceptedTypes);
+
+  return respond(request, response, resp.content, resp.type, 404);
 };
 
 
-module.exports = {getIndex,success,badRequest,unauthorized,forbidden,internal,notImplemented,notFound,};
+module.exports = {
+  getIndex,
+  getCSS,
+  success,
+  badRequest,
+  unauthorized,
+  forbidden,
+  internal,
+  notImplemented,
+  notFound,
+};
--- src/server.js
+++ src/server.js
@@ -1,27 +1,36 @@
 const http = require('http');
 const url = require('url');
 const responseHandler = require('./responses.js');
+const query = require('querystring');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-    '/':responseHandler.getIndex,
-    '/success':responseHandler.success,
-    '/badRequest':responseHandler.badRequest,
-    '/unauthorized':responseHandler.unauthorized,
-    '/forbidden':responseHandler.forbidden,
-    '/internal':responseHandler.internal,
-    '/notImplemented':responseHandler.notImplemented,
-    notFound: responseHandler.notFound
+  '/': responseHandler.getIndex,
+  '/style.css': responseHandler.getCSS,
+  '/success': responseHandler.success,
+  '/badRequest': responseHandler.badRequest,
+  '/unauthorized': responseHandler.unauthorized,
+  '/forbidden': responseHandler.forbidden,
+  '/internal': responseHandler.internal,
+  '/notImplemented': responseHandler.notImplemented,
+  notFound: responseHandler.notFound,
 };
 
 const onRequest = (request, response) => {
   const parsedURL = url.parse(request.url);
-  
-  if(urlStruct[parsedUrl.pathname]){
-    urlStruct[parsedUrl.pathname](request,response,accpetedTypes);
+
+  const accpetedTypes = request.headers.accept.split(',');
+  const params = query.parse(parsedURL.query);
+
+  if (urlStruct[parsedURL.pathname]) {
+    urlStruct[parsedURL.pathname](request, response, accpetedTypes, params);
+  } else{
+    urlStruct.notFound(request, response, accpetedTypes, params);
   }
 };
 
 
 http.createServer(onRequest).listen(port);
+
+console.log(`listening for requests on 127.0.0.1:${port}`);
